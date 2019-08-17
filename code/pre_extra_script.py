08db6030c309d7b44709b1c5b1bbc604703c450c
“Jéan <jean@iotplay.org>
2019-08-17 14:17:58 +0200

    JR commit

--- code/pre_extra_script.py
+++ code/pre_extra_script.py
@@ -15,25 +15,12 @@ Import("env")
 env.Append(CPPDEFINES=[
   "PIO_FRAMEWORK_ARDUINO_ESPRESSIF_SDK22x",
 #  "CONTROLLER_SET_ALL",
-#  "NOTIFIER_SET_NONE",
+  "NOTIFIER_SET_NONE",
 #  "PLUGIN_SET_ONLY_SWITCH",
   "USES_P001",  # Switch
   "USES_C014",  # Homie Controller
   "USES_P086",  # Homie Receiver
   "USES_P005",  # DHT 22
-#  "USES_P002",  # ADC
-#  "USES_P004",  # Dallas DS18b20
-#  "USES_P028",  # BME280
-#  "USES_P036",  # FrameOLED
-#  "USES_P049",  # MHZ19
-#  "USES_P052",  # SenseAir
-#  "USES_P056",  # SDS011-Dust
-#  "USES_P059",  # Encoder
-#  "USES_P082",  # GPS
-#  "USES_P085",  # AcuDC24x
-#  "USES_P087",  # Serial Proxy
-
-  "USES_C018", 
 
   ("WEBSERVER_RULES_DEBUG", "0")
 ])
